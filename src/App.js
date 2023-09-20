2e2dd5687466e4e374c3913b9efa7fedac6c3de0
JoramWells <jorammanoah@gmail.com>
2023-09-20 08:22:41 -0700

    Added modal view

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './css/style.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
+import ModalFade from './components/ModalFade';
 
 // eslint-disable-next-line require-jsdoc
 function App() {
@@ -53,26 +54,7 @@ function App() {
       {/* Navbar & Carousel End */}
       <Carousel />
       {/* Full Screen Search Start */}
-      <div className="modal fade" id="searchModal" tabIndex={-1}>
-        <div className="modal-dialog modal-fullscreen">
-          <div className="modal-content"
-            style={{background: 'rgba(9, 30, 62, .7)'}}>
-            <div className="modal-header border-0">
-              <button type="button" className="btn bg-white
-               btn-close" data-bs-dismiss="modal" aria-label="Close" />
-            </div>
-            <div className="modal-body d-flex align-items-center
-            justify-content-center">
-              <div className="input-group" style={{maxWidth: '600px'}}>
-                <input type="text" className="form-control bg-transparent
-                 border-primary p-3" placeholder="Type search keyword" />
-                <button className="btn btn-primary
-                px-4"><i className="bi bi-search" /></button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ModalFade/>
       {/* Full Screen Search End */}
       {/* Facts Start */}
       <div className="container-fluid facts py-5 pt-lg-0">
