137dc554864c5a124321f125ee5fb6d571484766
JoramWells <jorammanoah@gmail.com>
2023-09-20 08:37:11 -0700

    Added modal view

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Service from './components/Service';
 import Pricing from './components/Pricing';
 import Footer from './components/Footer';
 import Blog from './components/Blog';
+import Vendor from './components/Vendor';
 
 // eslint-disable-next-line require-jsdoc
 function App() {
@@ -78,23 +79,7 @@ function App() {
       <Blog/>
       {/* Blog Start */}
       {/* Vendor Start */}
-      <div className="container-fluid py-2 wow fadeInUp" data-wow-delay="0.1s">
-        <div className="container py-2 mb-5">
-          <div className="bg-white">
-            <div className="owl-carousel vendor-carousel">
-              <img src="img/vendor-1.jpg" alt="" />
-              <img src="img/vendor-2.jpg" alt="" />
-              <img src="img/vendor-3.jpg" alt="" />
-              <img src="img/vendor-4.jpg" alt="" />
-              <img src="img/vendor-5.jpg" alt="" />
-              <img src="img/vendor-6.jpg" alt="" />
-              <img src="img/vendor-7.jpg" alt="" />
-              <img src="img/vendor-8.jpg" alt="" />
-              <img src="img/vendor-9.jpg" alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Vendor/>
       {/* Vendor End */}
       {/* Footer Start */}
       <Footer />
---
+++ src/components/Vendor.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Vendor = () => {
+  return (
+    <div className="container-fluid py-2 wow fadeInUp" data-wow-delay="0.1s">
+      <div className="container py-2 mb-5">
+        <div className="bg-white">
+          <div className="owl-carousel vendor-carousel">
+            <img src="img/vendor-1.jpg" alt="" />
+            <img src="img/vendor-2.jpg" alt="" />
+            <img src="img/vendor-3.jpg" alt="" />
+            <img src="img/vendor-4.jpg" alt="" />
+            <img src="img/vendor-5.jpg" alt="" />
+            <img src="img/vendor-6.jpg" alt="" />
+            <img src="img/vendor-7.jpg" alt="" />
+            <img src="img/vendor-8.jpg" alt="" />
+            <img src="img/vendor-9.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Vendor;
