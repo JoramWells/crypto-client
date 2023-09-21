74688b253ae124ac603cf672f5efc178319ba336
JoramWells <jorammanoah@gmail.com>
2023-09-20 23:09:30 -0700

    Changed to yarn

--- src/components/Vendor.js
+++ src/components/Vendor.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import vendor1 from '../img/vendor-1.jpg';
+import vendor2 from '../img/vendor-2.jpg';
+import vendor3 from '../img/vendor-3.jpg';
+import vendor4 from '../img/vendor-4.jpg';
+import vendor5 from '../img/vendor-5.jpg';
+import vendor6 from '../img/vendor-6.jpg';
+import vendor7 from '../img/vendor-7.jpg';
+import vendor8 from '../img/vendor-8.jpg';
+import vendor9 from '../img/vendor-9.jpg';
+
 
 const Vendor = () => {
   return (
@@ -6,15 +15,15 @@ const Vendor = () => {
       <div className="container py-2 mb-5">
         <div className="bg-white">
           <div className="owl-carousel vendor-carousel">
-            <img src="img/vendor-1.jpg" alt="" />
-            <img src="img/vendor-2.jpg" alt="" />
-            <img src="img/vendor-3.jpg" alt="" />
-            <img src="img/vendor-4.jpg" alt="" />
-            <img src="img/vendor-5.jpg" alt="" />
-            <img src="img/vendor-6.jpg" alt="" />
-            <img src="img/vendor-7.jpg" alt="" />
-            <img src="img/vendor-8.jpg" alt="" />
-            <img src="img/vendor-9.jpg" alt="" />
+            <img src={vendor1} alt="vendor1" />
+            <img src={vendor2} alt="vendor2" />
+            <img src={vendor3} alt="vendor3" />
+            <img src={vendor4} alt="vendor4" />
+            <img src={vendor5} alt="vendor5" />
+            <img src={vendor6} alt="vendor6" />
+            <img src={vendor7} alt="vendor7" />
+            <img src={vendor8} alt="vendor8" />
+            <img src={vendor9} alt="vendor9" />
           </div>
         </div>
       </div>
